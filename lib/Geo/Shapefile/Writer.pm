6c371c19455b46e5b2f55b07396cf98472b61d19
xliosha@gmail.com <xliosha@gmail.com@a1545a6d-7d38-dfce-bdf8-0bb74e1ab084>
2012-04-26 11:24:28 +0000

    refactoring ancient code

--- lib/Geo/Shapefile/Writer.pm
+++ lib/Geo/Shapefile/Writer.pm
@@ -11,12 +11,12 @@ use autodie;
 use Carp;
 
 use CAM::DBF;
+use List::Util qw/ min max /;
 
 
 
-my $INF = 10e20;
-
-my %shapetype = (
+my %shape_type = (
+    # extend
     NULL        => 0,
     POINT       => 1,
     POLYLINE    => 3,
@@ -24,123 +24,164 @@ my %shapetype = (
 );
 
 
+=method new
+    
+    my $shp_writer = Geo::Shapefile::Writer->new( $name, $type, @dbf_fields );
+
+Constructor.
+Creates object and 3 associated files
+
+=cut
+
 sub new {
     my $class = shift;
-    my $self  = {};
+    my ($name, $type, @dbf_fields) = @_;
 
-    $self->{NAME} = shift;
-    $self->{SHPSIZE} = 50;
-    $self->{SHXSIZE} = 50;
-    $self->{RCOUNT} = 0;
+    my $shape_type = $shape_type{ uc($type || q{}) };
+    croak "Invalid shape type: $type"  if !defined $shape_type;
 
-    $self->{XMIN} = $INF;
-    $self->{XMAX} = -$INF;
-    $self->{YMIN} = $INF;
-    $self->{YMAX} = -$INF;
-    
-    $self->{TYPE} = $shapetype{uc shift};
-    croak 'Shapefile type not specified'        if !$self->{TYPE};
+    my $self = bless {
+        NAME     => $name,
+        TYPE     => $shape_type,
+        RCOUNT   => 0,
+        SHP_SIZE => 50,
+        SHX_SIZE => 50,
+    }, $class;
 
-    open (my $shp, '>', $self->{NAME}.'.shp');
-    croak "Can't create .shp"   if !$shp;
-    binmode $shp;
-    print $shp pack("Nx[20]NLLddddx[32]",( 9994,$self->{SHPSIZE},1000,$self->{TYPE},$self->{XMIN},$self->{YMIN},$self->{XMAX},$self->{YMAX} ));
-    $self->{SHP} = $shp;
+    my $header_data = $self->_get_header('SHP');
 
-    open (my $shx, '>', $self->{NAME}.'.shx');
-    croak "Can't create .shx"   if !$shx;
-    binmode $shx;
-    print $shx pack("Nx[20]NLLddddx[32]",( 9994,$self->{SHXSIZE},1000,$self->{TYPE},$self->{XMIN},$self->{YMIN},$self->{XMAX},$self->{YMAX} ));
-    $self->{SHX} = $shx;
+    open $self->{SHP}, '>:raw', "$name.shp";
+    print {$self->{SHP}} $header_data; 
 
-    my $dbf = CAM::DBF->create($self->{NAME}.'.dbf', @_);
+    open $self->{SHX}, '>:raw', "$name.shx";
+    print {$self->{SHX}} $header_data; 
+
+    my $dbf = CAM::DBF->create( "$name.dbf", @dbf_fields );
 #    $dbf->writeHeader();
     $self->{DBF} = $dbf;
 
-
-    bless ($self, $class);
     return $self;
 }
 
 
+{
+my $header_size = 100;
+# position, pack_type, object_field, default
+my @header_fields = (
+    [ 0,  'N', undef,   9994 ], # magic
+    [ 24, 'N', _SIZE => $header_size / 2 ], # file size in 16-bit words
+    [ 28, 'L', undef,   1000 ], # version
+    [ 32, 'L', 'TYPE' ],
+    [ 36, 'd', 'XMIN' ],
+    [ 40, 'd', 'YMIN' ],
+    [ 44, 'd', 'XMAX' ],
+    [ 48, 'd', 'YMAX' ],
+);
+
+sub _get_header {
+    my ($self, $file_type) = @_;
+
+    my @use_fields =
+        grep { defined $_->[2] }
+        map {[ $_->[0], $_->[1], $_->[2] && ($self->{$_->[2]} // $self->{"$file_type$_->[2]"}) // $_->[3] ]}
+        @header_fields;
+
+    my $pack_string = join q{ }, map { sprintf '@%d%s', @$_ } (@use_fields, [$header_size, q{}]);
+    return pack $pack_string, map { $_->[2] } @use_fields;
+}
+}
 
 
+=method add_shape
 
-sub write {
-    my $self = shift;
+    $shp_writer->add_shape( $object, @attributes );
 
-    my ($xmin,$ymin,$xmax,$ymax) = ($INF,$INF,-$INF,-$INF);
+=cut
 
-    my $data = shift;
-    my $rdata = q{};
+sub add_shape {
+    my ($self, $data, @attributes) = @_;
 
-    ##  null
-    if ( $self->{TYPE} == $shapetype{NULL} ) {
-        $rdata = pack( 'L', $self->{TYPE} );
-    }
+    my ($xmin, $ymin, $xmax, $ymax);
 
-    ##  point
-    if ( $self->{TYPE} == $shapetype{POINT} ) {
-        $rdata = pack( 'Ldd', $self->{TYPE}, @$data );
-    }
-
-    ##  polyline or polygon
-    if ( $self->{TYPE} ~~ [ @shapetype{'POLYLINE','POLYGON'} ] ) {
-        my $rpart = q{};
-        my $rpoint = q{};
-        my $ipoint = 0;
-        for my $line (@{$data}) {
-            $rpart .= pack 'L', $ipoint;
-            for my $point (@{$line}) {
-                my ($x, $y) = @{$point}[0..1];
-                $rpoint .= pack 'dd', $x,$y;
-                $ipoint ++;
-                $xmin = $x      if $x<$xmin;
-                $ymin = $y      if $y<$ymin;
-                $xmax = $x      if $x>$xmax;
-                $ymax = $y      if $y>$ymax;
-            }
+    my $rdata;
+    given ( $self->{TYPE} ) {
+        when ( $shape_type{NULL} ) {
+            $rdata = pack( 'L', $self->{TYPE} );
         }
-        $rdata = pack( 'LddddLL', $self->{TYPE}, $xmin,$ymin,$xmax,$ymax, scalar(@{$data}), $ipoint ) . $rpart . $rpoint;
+
+        when ( $shape_type{POINT} ) {
+            $rdata = pack( 'Ldd', $self->{TYPE}, @$data );
+            ($xmin, $ymin, $xmax, $ymax) = ( @$data, @$data );
+        }
+
+        when ( [ @shape_type{'POLYLINE','POLYGON'} ] ) {
+            my $rpart = q{};
+            my $rpoint = q{};
+            my $ipoint = 0;
+
+            for my $line ( @$data ) {
+                $rpart .= pack 'L', $ipoint;
+                for my $point ( @$line ) {
+                    my ($x, $y) = @$point;
+                    $rpoint .= pack 'dd', $x, $y;
+                    $ipoint ++;
+                }
+            }
+
+            $xmin = min map {$_->[0]} map {@$_} @$data;
+            $ymin = min map {$_->[1]} map {@$_} @$data;
+            $xmax = max map {$_->[0]} map {@$_} @$data;
+            $ymax = max map {$_->[1]} map {@$_} @$data;
+
+            $rdata = pack 'LddddLL', $self->{TYPE}, $xmin, $ymin, $xmax, $ymax, scalar @$data, $ipoint;
+            $rdata .= $rpart . $rpoint;
+        }
     }
 
     $self->{RCOUNT} ++;
 
-    my $shx = $self->{SHX};
-    print $shx pack 'NN', $self->{SHPSIZE}, length($rdata)/2;
-    $self->{SHXSIZE} += 4;
+    print {$self->{SHX}} pack 'NN', $self->{SHP_SIZE}, length($rdata)/2;
+    $self->{SHX_SIZE} += 4;
 
-    my $shp = $self->{SHP};
-    print $shp pack 'NN', $self->{RCOUNT}, length($rdata)/2;
-    print $shp $rdata;
+    print {$self->{SHP}} pack 'NN', $self->{RCOUNT}, length($rdata)/2;
+    print {$self->{SHP}} $rdata;
+    $self->{SHP_SIZE} += 4+length($rdata)/2;
 
-    $self->{SHPSIZE} += 4+length($rdata)/2;
-    $self->{XMIN} = $xmin       if  $xmin < $self->{XMIN} ;
-    $self->{YMIN} = $ymin       if  $ymin < $self->{YMIN} ;
-    $self->{XMAX} = $xmax       if  $xmax > $self->{XMAX} ;
-    $self->{YMAX} = $ymax       if  $ymax > $self->{YMAX} ;
+    $self->{DBF}->appendrow_arrayref( \@attributes );
 
-    $self->{DBF}->appendrow_arrayref( \@_ );
+    $self->{XMIN} = min grep {defined} ($xmin, $self->{XMIN});
+    $self->{YMIN} = min grep {defined} ($ymin, $self->{YMIN});
+    $self->{XMAX} = max grep {defined} ($xmax, $self->{XMAX});
+    $self->{YMAX} = max grep {defined} ($ymax, $self->{YMAX});
+
+    return $self;
 }
 
 
+=merhod finalize
 
+    $shp_writer->finalize();
 
+Update global fields, close files
+
+=cut
 
 sub finalize {
     my $self = shift;
 
     my $shp = $self->{SHP};
     seek $shp, 0, 0;
-    print $shp pack("Nx[20]NLLddddx[32]",( 9994,$self->{SHPSIZE},1000,$self->{TYPE},$self->{XMIN},$self->{YMIN},$self->{XMAX},$self->{YMAX} ));
+    print {$shp} $self->_get_header('SHP');
     close $shp;
 
     my $shx = $self->{SHX};
     seek $shx, 0, 0;
-    print $shx pack("Nx[20]NLLddddx[32]",( 9994,$self->{SHXSIZE},1000,$self->{TYPE},$self->{XMIN},$self->{YMIN},$self->{XMAX},$self->{YMAX} ));
+    print {$shx} $self->_get_header('SHX');
     close $shx;
 
     $self->{DBF}->closeDB();
+
+    return;
 }
 
 1;
