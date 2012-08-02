501ecf11dd2a0968c7b4feb9fbaf992e452ad7d0
xliosha@gmail.com <xliosha@gmail.com@a1545a6d-7d38-dfce-bdf8-0bb74e1ab084>
2012-08-02 06:36:20 +0000

    reverted

--- trunk/lib/Geo/Shapefile/Writer.pm
+++ trunk/lib/Geo/Shapefile/Writer.pm
@@ -1,7 +1,13 @@
-package Geo::Shapefile::Writer;
-# ABSTRACT: simple pureperl shapefile writer
+use 5.010;
+use strict;
+use warnings;
 
 # $Id$
+
+package Geo::Shapefile::Writer;
+
+# NAME: Geo::Shapefile::Writer
+# ABSTRACT: simple pureperl shapefile writer
 
 =head1 DESCRIPTION
 
@@ -23,9 +29,6 @@ So here is an alternative, if you need just simple shp export.
 
 =cut
 
-use 5.010;
-use strict;
-use warnings;
 
 use utf8;
 use autodie;
